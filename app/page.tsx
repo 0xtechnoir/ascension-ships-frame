ed20b655a9d5ac8ae44d0471535cac2d36c99609
0xNaylor <0xnaylor@gmail.com>
2024-03-21 14:53:52 +0000

    Updated ship images

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmWmaCSxQ4uJ83QjiYMvumW14zNyc7JhrZWapvB8zy2qE5/ship_1.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNf4W8kmL9B58skK5aSMdijGmZ1MTLFQ1r6weJCJEFoGS/ship_1.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Ascension Ships',
     description: 'A frame showcasing ships coming to Ascension soon',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmWmaCSxQ4uJ83QjiYMvumW14zNyc7JhrZWapvB8zy2qE5/ship_1.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNf4W8kmL9B58skK5aSMdijGmZ1MTLFQ1r6weJCJEFoGS/ship_1.png`],
   },
   other: {
     ...frameMetadata,
